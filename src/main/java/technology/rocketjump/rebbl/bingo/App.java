08b5a1f796c22b5c0d01a946458afc42473f77d1
Ross Taylor-Turner <ross.turner001@googlemail.com>
2018-10-10 12:54:25 +0100

    Initial commit

---
+++ src/main/java/technology/rocketjump/rebbl/bingo/App.java
@@ -0,0 +1,13 @@
+package technology.rocketjump.rebbl.bingo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
